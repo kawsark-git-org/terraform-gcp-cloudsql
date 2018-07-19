57bcb623444ce24f2f267d917b98267687b2622a
kawsark <kawsar@hashicorp.com>
2018-07-19 14:30:11 -0400

    Initial commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+variable "gcp_project_name" {}
+variable "gcp_sql_user_name" {
+	 default = "root"
+}
+variable "gcp_sql_user_pw" {}
+
+variable "region"{
+  default = "us-central1"
+}
+
+provider "google" {
+  #Google provider credentials via environment variable: GOOGLE_CLOUD_KEYFILE_JSON
+  project = "${var.gcp_project_name}"
+  region  = "${var.region}"
+}
+
+data "http" "ip" {
+  url = "http://whatismyip.akamai.com/"
+}
+
+resource "google_sql_database_instance" "cloudsql-postgres-master" {
+  name = "master-instance"
+  database_version = "POSTGRES_9_6"
+  region = "${var.region}"
+
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+    ip_configuration {
+            ipv4_enabled = true
+            require_ssl = false
+            authorized_networks = {
+                name = "terraform-server-IP-whitelist"
+                value = "${data.http.ip.body}/32"
+            }
+        }
+  }
+}
+
+resource "google_sql_user" "users" {
+  name     = "${var.gcp_sql_user_name}"
+  instance = "${google_sql_database_instance.cloudsql-postgres-master.name}"
+  password = "${var.gcp_sql_user_pw}"
+}
+
+output "connection_name" {
+  value = "${google_sql_database_instance.cloudsql-postgres-master.connection_name}"
+}
+
+output "ip" {
+  value = "${google_sql_database_instance.cloudsql-postgres-master.ip_address.0.ip_address}"
+}
+
