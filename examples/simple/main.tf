9050c834966baaa912e96f96082fd54588b3d102
kawsark <kawsar@hashicorp.com>
2018-08-20 01:21:18 -0400

    Added database_name variable in module

--- examples/simple/main.tf
+++ examples/simple/main.tf
@@ -2,6 +2,11 @@ variable "gcp_credentials" {
   description = "GCP credentials needed by google provider"
 }
 
+variable "database_name" {
+  description = "Name of the Cloud SQL instance"
+  default = "example-gcp-cloudsql"
+}
+
 variable "region" {
   default = "us-central1"
 }
@@ -14,7 +19,7 @@ module "example-gcp-cloudsql" {
   gcp_credentials = "${var.gcp_credentials}"
   source = "github.com/kawsark-git-org/terraform-gcp-cloudsql"
   region  = "${var.region}"
-  database_name = "example-gcp-cloudsql"
+  database_name = "${var.database_name}"
   gcp_sql_root_user_pw = "${var.gcp_sql_root_user_pw}"
   authorized_network = "${var.authorized_network}"
 }
